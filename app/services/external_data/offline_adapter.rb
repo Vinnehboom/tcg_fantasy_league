a45ffc59114bcf042f521101b4fdc09ece93c306
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-06-11 11:42:04 +0200

    Add Devise with base User model

--- app/services/external_data/offline_adapter.rb
+++ app/services/external_data/offline_adapter.rb
@@ -12,11 +12,6 @@ module ExternalData
                                         'the #update_players method was called on an invalid adapter')
     end
 
-    def self.update_players
-      raise CustomException.new('Offline method called', self.class.name,
-                                'the #update_players method was called on an invalid adapter')
-    end
-
   end
 
 end
