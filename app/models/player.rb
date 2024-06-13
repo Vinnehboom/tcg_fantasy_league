7e8a5e1ec9113544c3def41a52a43b63451b3957
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-06-13 22:07:46 +0200

    Make Country optional for Players
    Not all platforms might support countries, nor do all players
    on limitless have a country linked to them

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,6 @@ class Player < ApplicationRecord
 
   validates :name, presence: true
   validates :external_id, presence: true
-  validates :country, presence: true
   validates :external_points, presence: true
   belongs_to :game
 
