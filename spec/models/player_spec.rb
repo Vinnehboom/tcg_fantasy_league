81181728b5011dcd8b02c1280e128ebd40ceba50
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-06-13 22:07:46 +0200

    Make Country optional for Players
    Not all platforms might support countries, nor do all players
    on limitless have a country linked to them

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Player do
   it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:country) }
   it { is_expected.to validate_presence_of(:external_id) }
   it { is_expected.to validate_presence_of(:external_points) }
 
