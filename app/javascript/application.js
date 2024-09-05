f750c6e12e1b7e17fb37948ccf4c1f074f87d0c7
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-09-05 17:58:59 +0200

    Fix issue with bootstrap overwriting all custom js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap"
