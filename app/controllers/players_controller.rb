775c971980fea53be84ca4d1753127dbf9cd83ed
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-09-06 16:57:52 +0200

    Allow users to filter players on the player index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,9 +2,28 @@ class PlayersController < ScopedGameController
 
   def index
     @players = @game.players.joins(:external_scores)
-                    .group('players.id')
-                    .order('max(external_scores.score) desc')
-                    .page(params[:page])
+    @countries = @players.pluck(:country).uniq.sort
+    @players = apply_filters(players: @players)
+               .group('players.id')
+               .order('max(external_scores.score) desc')
+    @players = @players.page(params[:page]).per(25)
+    @filters = filter_params
+  end
+
+  private
+
+  def filter_params
+    params.permit(:country, :name).compact_blank
+  end
+
+  def apply_filters(players:)
+    return players if filter_params.blank?
+
+    filter_params.to_h.each do |attribute, value|
+      players = players.and(players.contains(attribute, value))
+    end
+
+    players
   end
 
 end
