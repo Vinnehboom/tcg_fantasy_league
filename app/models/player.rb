1b79ccac0b6b2ff30300a7cd47685779690f8926
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-09-08 12:18:09 +0200

    Add score difference method to player

--- app/models/player.rb
+++ app/models/player.rb
@@ -13,11 +13,17 @@ class Player < ApplicationRecord
   end
 
   def latest_score_before(date:)
-    external_scores.order('created_at desc').where(created_at: ..date).first&.score
+    external_scores.order('created_at desc').where(created_at: ..date).first&.score || 0
   end
 
   def decorated_cost
     format('%0.02f', cost)
+  end
+
+  def score_difference(date:, other_date:)
+    score = latest_score_before(date:)
+    other_score = latest_score_before(date: other_date)
+    score - other_score
   end
 
   include ExternalResource
