63eab7ce7e987c3540ca540d0c89d8e78b204005
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2024-09-09 09:40:01 +0200

    Show region relevant players first for the draft's tournament

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -75,12 +75,28 @@ class RostersController < ScopedGameController
   end
 
   def set_page_variables
-    @players = @game.players.includes(:external_scores)
+    @players = @game.players
     @countries = @players.pluck(:country).uniq.sort
+    filter_players
+    @filters = filter_params
+  end
+
+  def filter_players
+    tournament_region = Country[@roster.tournament.country].region
+    region_countries = Country.collect_countries_with(tournament_region, :region).map(&:alpha2).reject { |c| c == 'IS' }
+    latest_scores = ExternalScore
+                    .select('external_scores.player_id, external_scores.score, external_scores.created_at')
+                    .from('external_scores, (SELECT player_id, ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY created_at DESC) AS rn FROM external_scores) ranked_scores') # rubocop:disable Layout/LineLength
+                    .where('external_scores.player_id = ranked_scores.player_id AND ranked_scores.rn = 1')
+
+    @players = Player
+               .left_joins(:external_scores)
+               .joins("LEFT JOIN (#{latest_scores.to_sql}) latest_scores ON latest_scores.player_id = players.id")
+               .order(Arel.sql("players.country = ANY(ARRAY['#{region_countries.join("', '")}']::text[]) DESC"))
+               .order('latest_scores.created_at ASC')
     @players = apply_filters(players: @players)
     @players = @players.page(params[:page]).per(25)
     @players.each { |player| player.cost = @roster.draft.cost_for(player:) }
-    @filters = filter_params
   end
 
 end
