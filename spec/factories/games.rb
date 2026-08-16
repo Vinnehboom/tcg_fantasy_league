57e06df1e11adbd219ac8374e2386f52ea6df1e1
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2026-08-16 10:14:27 +0000

    Fix intermittent games_pkey collisions in the :game factory
    id { "#{Faker::Game.title} #{rand(10)}" } drew from too small a pool
    to stay collision-free across a full suite run, throwing sporadic
    PG::UniqueViolation on unrelated specs. A sequence is collision-proof
    by construction; Game#id is a free-form string PK, not an
    autoincrement column, so FactoryBot/IdSequence's usual assumption
    doesn't hold here.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :game do
-    id { "#{Faker::Game.title} #{rand(10)}" }
+    sequence(:id) { |n| "GAME#{n}" } # rubocop:disable FactoryBot/IdSequence
     name { Faker::Game.title }
     base_uri { Faker::Internet.url }
   end
