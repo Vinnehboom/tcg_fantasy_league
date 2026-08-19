18cd27e5a0d496af3fa4a708b87bb7766443e969
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2026-08-19 11:20:52 +0000

    Rewrite LabsPlayers as an ApplicationService PORO
    Matches the style guide's initialize + #call standard for
    single-use-case services rather than the sibling scrapers' bare
    class-method pattern (those are getting deleted in B-3 anyway). The
    JsonApiClient is now held as constructor state and injectable.

--- app/services/external_data/pokemon/tcg/labs_players.rb
+++ app/services/external_data/pokemon/tcg/labs_players.rb
@@ -4,28 +4,32 @@ module ExternalData
 
     module Tcg
 
-      class LabsPlayers
+      class LabsPlayers < ApplicationService
 
         MEW_BASE_URI = 'https://mew.limitlesstcg.com'.freeze
         RANKINGS_PATH = '/labs/data/tcg/rankings'.freeze
         DIVISION = 'MA'.freeze
 
-        def self.all(season:)
+        def initialize(season:, client: ExternalData::JsonApiClient.new(base_uri: MEW_BASE_URI))
+          @season = season
+          @client = client
+          super()
+        end
+
+        def call
           response = client.get_json(path: RANKINGS_PATH, query: { season:, division: DIVISION })
           parse_players(response)
         end
 
-        def self.client
-          ExternalData::JsonApiClient.new(base_uri: MEW_BASE_URI)
-        end
-        private_class_method :client
+        private
 
-        def self.parse_players(response)
+        attr_reader :season, :client
+
+        def parse_players(response)
           response.fetch('message', []).filter_map { |entry| build_player(entry) }
         end
-        private_class_method :parse_players
 
-        def self.build_player(entry)
+        def build_player(entry)
           player_id = entry['player_id']
           points = entry['points']
           return if player_id.blank? || points.blank?
@@ -39,7 +43,6 @@ module ExternalData
             }
           )
         end
-        private_class_method :build_player
 
       end
 
