2710aae20d9f4fb80b3f37dd9d80dde3860925f2
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2026-08-25 18:42:44 +0000

    Add ExternalData::Ptcg::ImportResultsJob

---
+++ app/jobs/external_data/ptcg/import_results_job.rb
@@ -0,0 +1,44 @@
+module ExternalData
+
+  module Ptcg
+
+    class ImportResultsJob < ExternalData::ImportJob
+
+      def perform(tournament_id:)
+        @tournament_id = tournament_id
+        run_import
+      end
+
+      private
+
+      def game
+        Game.find('PTCG')
+      rescue ActiveRecord::RecordNotFound
+        raise "#{self.class.name}: no Game row with id 'PTCG' — seed it before running this job."
+      end
+
+      def adapter
+        @adapter ||= ExternalData::Pokemon::Tcg::Adapter.new(game:)
+      end
+
+      def kind
+        :results
+      end
+
+      def fetch(interface)
+        interface.update_results(tournament:, field_size: adapter.field_size(tournament:))
+      end
+
+      def requestable
+        tournament
+      end
+
+      def tournament
+        @tournament ||= ::Tournament.find(@tournament_id)
+      end
+
+    end
+
+  end
+
+end
