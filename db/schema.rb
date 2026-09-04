7311061fd8ec813c1909d46355dbd15d74905db0
Vinnehboom <vinnie.schelfhaut.95@hotmail.com>
2026-09-04 23:27:00 +0000

    Regenerate schema.rb from main + this branch's own migration only

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_09_02_120000) do
+ActiveRecord::Schema[7.1].define(version: 2026_09_03_090000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -160,7 +160,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_09_02_120000) do
 
   create_table "settings", force: :cascade do |t|
     t.string "settingable_type", null: false
-    t.bigint "settingable_id", null: false
+    t.string "settingable_id", null: false
     t.jsonb "settings", default: {}, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
